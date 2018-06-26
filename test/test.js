292cceee18a0cb708013c1ed4a4040d46bbb9f80
Brian <brian@pumapay.io>
2018-06-26 03:46:15 -0700

    fix api key in testing

--- test/test.js
+++ test/test.js
@@ -31,7 +31,7 @@ merchantWithoutApiKey.authenticate('test_user1200', 'pass0wrd').then(res => {
 // when request with apikey
 const merchantWithApiKey = new MerchantSDK({
     apiUrl: 'http://localhost:8081/api/v1',
-    apiKey: '1234567890'
+    apiKey: '170ca4231ad5cda7f9fb5a8319eab0058de55f908e8c5784b879cedbd3f86e3acbf0aa4b31a3e689ee740c5d36b1b11e687c19b7de7d79e723ca16a63c6ef9bf' //valid api key
 });
 merchantWithApiKey.authenticate('test_user1200', 'pass0wrd').then(res => {
     merchantWithApiKey.getRequest('/exchange/global').then(res => 
