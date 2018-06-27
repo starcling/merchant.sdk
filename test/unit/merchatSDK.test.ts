00b86e06da9a9dd2614902b6d4494625eafbe7fa
Brian <brian@pumapay.io>
2018-06-27 00:07:14 -0700

    Fix some unit tests

--- test/unit/merchatSDK.test.ts
+++ test/unit/merchatSDK.test.ts
@@ -27,7 +27,7 @@ describe('Connect Merchant Core API with username and password', () => {
         userToken.should.have.property('pmaUserToken').to.be.an('string');
         userToken.should.have.property('pmaApiKey').to.be.an('string');
     });
-    it('should return response from /exchange/global endpoint', async () => {
+    it('should return response from GET request to /exchange/global endpoint', async () => {
         const response = await merchantWithoutApiKey.getRequest('/exchange/global');
         response.should.have.property('success').that.is.equal(true);
         response.should.have.property('status').that.is.equal(200);
@@ -35,7 +35,7 @@ describe('Connect Merchant Core API with username and password', () => {
         response.should.have.property('data').to.be.an('object');
     });
 
-    it('should return response from /schedule endpoint', async () => {
+    it('should return response from POST request to /schedule endpoint', async () => {
         const response = await merchantWithoutApiKey.postRequest('/schedule', {
             "signature": "",
             "signatory_address": "A",
